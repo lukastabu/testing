1ea2581f7d767d5c2bd5823c34c5f7dc80407813
lukas.tabulevicius <lukas.tabulevicius@cortlex.com>
2022-11-07 17:11:26 +0200

    adding integration testing

---
+++ src/dependencies/complex.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\dependencies;
+
+use App\simple\simpleClass;
+
+class complex extends simpleClass
+{
+    public function calculateFinal($num1, $num2)
+    {
+
+    }
+}
---
+++ tests/Integration/testComplex.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Tests\Integration;
+
+use PHPUnit\Framework\TestCase;
+
+final class testSimple extends TestCase
+{
+
+}
